d46d9a7e9dff236d377fa5eabc27ce67591c54a0
Rafael Diniz <rafael@riseup.net>
2022-10-13 12:14:00 +0300

    added initial scripts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hermes-qa
 Hermes Quality Assesment Research
+
+This repository contains scripts for audio and still image
+quality assessment.
+
---
+++ scripts/run_image_tests.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+CODECS=vvc
+
+SOURCE_DIR=../data-set/image
+ENCODED_DIR=../artifacts/encoded-vvc
+RECONSTRUCTED_DIR=../artifacts/reconstructed-vvc
+
+COMPRESS_IMAGE=compress_image.sh
+DECOMPRESS_IMAGE=decompress_image.sh
+
+cd ${SOURCE_DIR}
+
+mkdir -p ${ENCODED_DIR}/
+mkdir -p ${RECONSTRUCTED_DIR}/
+
+rm -f ${ENCODED_DIR}/*
+rm -f ${RECONSTRUCTED_DIR}/*
+
+# compress data
+for i in *; do
+
+    no_extension=${i%.*}
+
+    input_file=\"${i}\"
+    output_file=\"${ENCODED_DIR}/${no_extension}.${CODEC}\"
+    echo "==== INPUT: ${input_file}"
+    echo "==== OUTPUT: ${output_file}"
+    eval ${COMPRESS_IMAGE} \"${i}\" \"${ENCODED_DIR}/${no_extension}.${CODEC}\"
+
+    echo "==== DONE"
+done
+
+cd ${ENCODED_DIR}
+
+# decompress data
+for i in *; do
+
+    no_extension=${i%.*}
+
+#    input_file=\"${i}\"
+#    output_file=\"${ENCODED_DIR}/${no_extension}.${CODEC}\"
+#    echo compress ${COMPRESS_IMAGE} ${input_file} ${output_file}
+    eval ${DECOMPRESS_IMAGE} \"${i}\" \"${RECONSTRUCTED_DIR}/${no_extension}.jpg\"
+
+done
+
+# TODO
+# calculate SSIM and PSNR metrics at least
